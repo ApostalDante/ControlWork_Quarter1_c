d8ec622aba4bf50320b5cd3cf5ab746e99ae7b27
ApostalDante <apostal666666@gmail.com>
2022-09-19 17:01:29 +0300

    finish Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿// Функция подсчета подходящих элементов
+int CountElement(string[] array)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+            size++;
+    }
+    return size;
+}
+
+// Функция формирования нового массива с заданными элементами по условию задачи
+string[] ConvertArray(string[] array, int size)
+{
+    int j = 0;
+    string[] resultArray = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+// Код программы
+string[] originalArray = { "Russia", "Denmark", "Kazan" };
+int sizeArray = CountElement(originalArray);
+string[] finalArray = new string[sizeArray];
+finalArray = ConvertArray(originalArray, sizeArray);
+Console.WriteLine(string.Join(", ", finalArray));
